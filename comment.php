5a4ca02407853cd5a1fe0bb16ef93f453f63a9fc
JennJin <JenniferJinhong@gmail.com>
2014-04-21 17:28:27 -0700

    Delete comment.php

--- comment.php
+++
@@ -1,5 +0,0 @@
-<?php
-# Loads comment_body.php and Creates Hook
-
-$wgAutoloadClasses['comment'] = $IP. '/extensions/Comment/comment_body.php';
-$wgHooks['ParserFirstCallInit'][] = 'comment::onParserInit';
